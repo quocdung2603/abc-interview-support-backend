434c671acd21f4a66d88f74f4108199f2c4728a3
MinTrees <2224802010382@student.tdmu.edu.vn>
2025-08-27 18:33:32 +0700

    bug 401

--- auth-service/src/main/java/com/auth/service/AuthServiceApplication.java
+++ auth-service/src/main/java/com/auth/service/AuthServiceApplication.java
@@ -2,8 +2,10 @@ package com.auth.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients(basePackages = "com.auth.service.client")
 public class AuthServiceApplication {
 
 	public static void main(String[] args) {
