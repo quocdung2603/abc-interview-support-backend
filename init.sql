8cef04038ae8ef8e6221ed82ad2de99089fc9cad
MinTrees <2224802010382@student.tdmu.edu.vn>
2025-08-27 21:40:51 +0700

    auth/fix-401 and gateway

--- init.sql
+++ init.sql
@@ -3,8 +3,16 @@ CREATE DATABASE authdb;
 CREATE DATABASE examdb;
 CREATE DATABASE questiondb;
 CREATE DATABASE careerdb;
-INSERT INTO role (role_name, description)
-VALUES
-    ('STUDENT', 'Role cho sinh viên'),
-    ('RECRUITER', 'Role cho nhà tuyển dụng'),
-    ('ADMIN', 'Role cho quản trị viên');
+-- Tạo bảng roles
+CREATE TABLE IF NOT EXISTS roles (
+                                     id          BIGSERIAL PRIMARY KEY,
+                                     role_name   VARCHAR(32) NOT NULL UNIQUE,
+    description TEXT
+    );
+
+-- Seed dữ liệu (bỏ qua nếu đã tồn tại)
+INSERT INTO roles(role_name, description) VALUES
+                                              ('STUDENT',   'Role cho sinh viên'),
+                                              ('RECRUITER', 'Role cho nhà tuyển dụng'),
+                                              ('ADMIN',     'Role cho quản trị viên')
+    ON CONFLICT (role_name) DO NOTHING;
